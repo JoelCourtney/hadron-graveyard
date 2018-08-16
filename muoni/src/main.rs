02a55d3380d433f15c33c86bfd890798928589b4
Joel Courtney <joel.e.courtney@gmail.com>
2018-08-15 23:31:34 -0600

    traits. traits. traits.....

--- muoni/src/main.rs
+++ muoni/src/main.rs
@@ -1,4 +1,8 @@
 extern crate regex;
+extern crate nalgebra as na;
+extern crate num_bigint as ni;
+extern crate num_complex as nc;
+extern crate num_traits;
 
 #[macro_use]
 extern crate lazy_static;
@@ -11,6 +15,9 @@ mod ast;
 mod lexer;
 mod parser;
 mod interpreter;
+
+use interpreter::value::{Number::*,Value::*,Unit};
+use interpreter::value;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -27,8 +34,16 @@ fn main() {
     let controls = parser::parse(lexemes);
 
     let mut test = ast::RValue::Number(5.);
-    let asdf = interpreter::value::Value::from(&test);
+    let asdf = value::Value::from(&test);
     test = ast::RValue::Number(6.);
     println!("{:?}",asdf);
     println!("{:?}",test);
+    let test2 = N(RI(i64::max_value()));
+    let test3 = N(RI(5));
+    let asdf2 = value::add(&test2,&test3);
+    println!("{:?}",test2);
+    println!("{:?}",test3);
+    println!("{:?}",asdf2);
+    println!("{:?}",value::add(&(N(RF(5.))),&asdf2));
+    println!("{:?}",na::DMatrix::from_element(2,3,5));
 }
